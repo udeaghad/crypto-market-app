13aad98b9369fe647111226634c8e8b3b6ef06ce
udeaghad <dozie.udeagha@gmail.com>
2022-08-05 10:57:06 +0100

    Perform test on filter reducer

--- src/Components/__test__/page.test.js
+++ src/Components/__test__/page.test.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import store from '../../Redux/ConfigureStore';
 import HomePage from '../HomePage';
 import CoinDetailsRender from '../CoinDetailsRender';
+import {getCoinsReducer} from '../../Redux/CoinReducer'
+import {searchByRank} from '../../Redux/CoinAPI'
 
 describe('To take snapshot of all components', () => {
   test('To Test HomePage component if renders successfully!', () => {
@@ -13,10 +15,36 @@ describe('To take snapshot of all components', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  test('To Test CoinPage component if renders successfully!', () => {
+  test('To Test CoinDetailRender component if renders successfully!', () => {
     const tree = renderer
       .create(<Provider store={store}><CoinDetailsRender /></Provider>)
       .toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  test('To test the filter function of the reducer', () => {
+    const state = {
+      coin:[
+      {id:'binance', name: 'Bitcoin', rank: 1, price: 23000},
+      {id: 'ethereum', name:'Ethereum', rank:2, price: 1800},
+      {id: 'tether', name: 'Tether', rank:3, price: 1},
+      {id: 'bnb', name: 'BNB', rank: 4, price: 307 },
+      {id: 'usd_coin', name: 'USD Coin', rank: 5, price: 0.8},
+      ],    
+      currency: 'USD'
+    
+    };
+
+    expect(getCoinsReducer(state, searchByRank(3))).toEqual(
+      {
+        coin:[
+        {id:'binance', name: 'Bitcoin', rank: 1, price: 23000},
+        {id: 'ethereum', name:'Ethereum', rank:2, price: 1800},
+        {id: 'tether', name: 'Tether', rank:3, price: 1},        
+        ],      
+        currency: 'USD'
+      }
+      
+    );
   });
 });

bb5e97eff68edd81bb811c98281915bb05dcba82
udeaghad <dozie.udeagha@gmail.com>
2022-08-06 09:13:22 +0100

    Remove the commented lines

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -98,20 +98,7 @@ const HomePage = () => {
       <div className="coin-heading">
         <div>
           <h4>Price Per Coin</h4>
-
-          {/* <form onSubmit={(e) => onSearchCurrency(e)}>
-            <input
-              name="search"
-              type="text"
-              className="search-field"
-              placeholder="Search By Currency"
-              onChange={(e)=> getSerachValue(e)}
-              onKeyDown={(e) => onBackspace(e)}
-            />
-            <button className="filter-btn" type="submit">Search</button>
-            <button className="clear-btn" type="button" onClick={(e) => onClear(e)}>Clear</button>
-          </form> */}
-        </div>
+      </div>
 
         <div className="search-row">
 
@@ -122,7 +109,7 @@ const HomePage = () => {
             onChange={(e) => onSelect(e)}
           >
             <option value="USD">Choose Currency</option>
-            <option value="USD">USD - US Dollars</option>
+            <option value='USD'>USD - US Dollars</option>
             <option value="EUR">EUR - EURO</option>
             <option value="GBP">GBP - British Pounds</option>
             <option value="CAD">CAD - Canadian Dollars</option>
